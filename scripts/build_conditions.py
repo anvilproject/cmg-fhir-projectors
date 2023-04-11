643fc3a0f9719c807b37964ebcab32693ff78419
Eric Torstenson <eric.s.torstenson@vumc.org>
2023-04-11 13:51:34 -0500

    Updated for clearer data structure

--- scripts/build_conditions.py
+++ scripts/build_conditions.py
@@ -9,28 +9,34 @@ import csv
 from term_lookup import pull_details
 import pdb
 from argparse import ArgumentParser
+import yaml
 
 motifs = {}
 
+
+
+
+
+
 subject_filenames = {
     "uw": {
-        "hmb-irb" : "../data/hmb-irb/uw_phs000693_hmb-irb_subject_2021-04-22_y9q6.csv",
-        "dw-ep" : "../data/dw-ep/uw_phs000693_ds-ep_subject_2021-04-22_y9q6.csv",
-        "gru-irb": "../data/gru-irb/uw_phs000693_gru-irb_subject_2021-04-23_y9q6.csv",
-        "ds-bav": "../data/ds-bav/uw_phs000693_ds-bav-irb-pub-rd_subject_2021-01-11_y9q5.csv",
-        "ds-nbia": "../data/ds-nbia/uw_phs000693_ds-nbia_subject_2021-01-13_y9q5.csv",
-        "hmb": "../data/hmb/uw_phs000693_hmb_subject_2021-02-09_y9q5.csv",
-        "gru": "../data/gru/gru-subject.csv"
+        "hmb-irb" : "data/uw/hmb-irb/uw_phs000693_hmb-irb_subject_2021-04-22_y9q6.csv",
+        "dw-ep" : "data/uw/dw-ep/uw_phs000693_ds-ep_subject_2021-04-22_y9q6.csv",
+        "gru-irb": "data/uw/gru-irb/uw_phs000693_gru-irb_subject_2021-04-23_y9q6.csv",
+        "ds-bav": "data/uw/ds-bav/uw_phs000693_ds-bav-irb-pub-rd_subject_2021-01-11_y9q5.csv",
+        "ds-nbia": "data/uw/ds-nbia/uw_phs000693_ds-nbia_subject_2021-01-13_y9q5.csv",
+        "hmb": "data/uw/hmb/uw_phs000693_hmb_subject_2021-02-09_y9q5.csv",
+        "gru": "data/uw/gru/gru-subject.csv"
     },
     "y": {
-        "ds-mc": "../data/ds-mc/Subject_CMG_Yale_phs000744_DS-MC_20201218_Y9Q1.csv",
-        "ds-rare": "../data/ds-rare/Subject_CMG_Yale_phs000744_DS-RARED_20201202_Y9Q1.csv",
-        "hmb": "../data/hmb/Subject_CMG_Yale_phs000744_HMB_20201218_Y9Q1.csv",
-        "gru": "../data/gru/Subject_CMG_Yale_phs000744_GRU_20201218_Y9Q1.csv"
+        "ds-mc": "data/yale/ds-mc/Subject_CMG_Yale_phs000744_DS-MC_20201218_Y9Q1.csv",
+        "ds-rare": "data/yale/ds-rare/Subject_CMG_Yale_phs000744_DS-RARED_20201202_Y9Q1.csv",
+        "hmb": "data/yale/hmb/Subject_CMG_Yale_phs000744_HMB_20201218_Y9Q1.csv",
+        "gru": "data/yale/gru/Subject_CMG_Yale_phs000744_GRU_20201218_Y9Q1.csv"
     },
     "bh": {
-        "hmb-npu": "../data/bh/hmb-npu/hmb-npu-subject.csv",
-        "hmb-irb-npu": "../data/bh/hmb-irb-npu/phs000711_HMB-IRB-NPU_CMG_BH_phs000711_HMBIRBNPU_Subject_ALL_Combined_FIXED_05Feb2021.csv"
+        "hmb-npu": "data/bh/hmb-npu/hmb-npu-subject.csv.dupes-removed",
+        "hmb-irb-npu": "data/bh/hmb-irb-npu/phs000711_HMB-IRB-NPU_CMG_BH_phs000711_HMBIRBNPU_Subject_ALL_Combined_FIXED_05Feb2021.csv"
     }
 }
 parser = ArgumentParser(description="Pull out HP Codes and pull the details from web")
@@ -48,7 +54,7 @@ filenames = subject_filenames[args.dataset]
 
 for consent in filenames:
     filename = filenames[consent]
-    with open(f"../output/conditions-{args.dataset}-{consent}.csv", "wt") as outf:
+    with open(f"output/conditions-{args.dataset}-{consent}.csv", "wt") as outf:
         writer = csv.writer(outf)
         writer.writerow([
             "subject_id",
@@ -71,36 +77,42 @@ for consent in filenames:
                         line[hp_content] = line[hp_content].replace("HP:0002664HP:0002715", "HP:0002664;HP:0002715")
                     codes = [x.strip() for x in line[hp_content].split(";")]
 
-                    
+                    observed = set(["-"])
                     for id in codes:
-                        details = pull_details(id)
+                        if id not in observed:
+                            print(id)
+                            details = pull_details(id)
 
-                        if details:
-                            writer.writerow([
-                                line['subject_id'],
-                                id,
-                                details.name,
-                                is_present
-                            ])
+                            if details:
+                                writer.writerow([
+                                    line['subject_id'],
+                                    id,
+                                    details.name,
+                                    is_present
+                                ])
 
-                            motifs[id] = [
-                                id,
-                                details.name,
-                                "subject",
-                                hp_content,
-                                "subject",
-                                details.code,
-                                details.name,
-                                details.system,
-                                ""
-                            ]
+                                motifs[id] = [
+                                    id,
+                                    details.name,
+                                    "subject",
+                                    hp_content,
+                                    "subject",
+                                    details.code,
+                                    details.name,
+                                    details.system,
+                                    ""
+                                ]
+                                observed.add(id)
+                            else:
+                                if id not in bad_ids:
+                                    print(f"Unable to find, {id}. Skipping it.")
+                                    bad_ids.add(id)
                         else:
-                            if id not in bad_ids:
-                                print(f"Unable to find, {id}. Skipping it.")
-                                bad_ids.add(id)
+                            print(f"Skipping duplicate code: {id}")
+
                     is_present = False
 
-with open(f"../output/hp-codes-{args.dataset}-ALL.csv", "wt") as f:
+with open(f"output/hp-codes-{args.dataset}-ALL.csv", "wt") as f:
     writer = csv.writer(f)
 
     writer.writerow(["local code",
